bdf4b155128a8ba43ccebb00e2069fdd3cfc4346
ykovalenko <wrestlerok99@gmail.com>
2019-05-16 10:20:01 +0300

    trying to fix webhook

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, All } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('telegram')
 export class TelegramController {
-  @Get('scedule')
+  @All('schedule')
   getSchedule(@Req() request: Request) {
     console.log(request);
   }

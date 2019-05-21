f2b62f5f2759bd255c683c31290ea6a9fca5137d
ykovalenko <wrestlerok99@gmail.com>
2019-05-21 09:55:22 +0300

    disabled polling

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -7,12 +7,13 @@ import { ScheduleService } from '../schedule/schedule.service';
 export class TelegramService {
   private readonly telegramBot: TelegramBot;
   private readonly webhookUrl: string = `${
-    process.env.APP_URL
+    process.env.APP_URL || 'https://awesome-nest-project-develop.herokuapp.com'
   }/telegram/schedule`;
   constructor(private schedule: ScheduleService) {
     const token = TelegramConfig.apiKey;
-    this.telegramBot = new TelegramBot(token, { polling: true });
+    this.telegramBot = new TelegramBot(token);
     this.telegramBot.setWebHook(this.webhookUrl);
+    console.log(this.webhookUrl);
     // this.telegramBot.onText(/[А-я|і]*-[\d]*/g, msg => {
     //   this.getNextLesson(msg.text).then(
     //     nextLesson => this.telegramBot.sendMessage(msg.chat.id, nextLesson),

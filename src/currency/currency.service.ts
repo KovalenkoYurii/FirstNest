a84cf1d0ec15cd8abe7d3e043aef95e102558019
ykovalenko <wrestlerok99@gmail.com>
2019-05-23 10:57:59 +0300

    added currency bot fixed

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -4,15 +4,19 @@ import { JSDOM } from 'jsdom';
 @Injectable()
 export class CurrencyService {
   private readonly currencyUrl: string;
-  constructor(private http: HttpService){
+  constructor(private http: HttpService) {
     this.currencyUrl = 'https://client-bank.privatbank.ua/p24/login';
   }
 
   public async getCurrency(currencyCode: string) {
     const {
       window: { document },
-    } = await JSDOM.fromURL(this.currencyUrl, {resources: 'usable', runScripts: 'dangerously'})
-    const currency = document.querySelector(`.${currencyCode}_box`).textContent || '';
+    } = await JSDOM.fromURL(this.currencyUrl, {
+      resources: 'usable',
+      runScripts: 'dangerously',
+    });
+    const currency =
+      document.querySelector(`.${currencyCode}_box`).textContent || '';
     return currency;
   }
 }
